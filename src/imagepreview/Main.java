4f388e41fcf014ac9d344cba6c6e62b64db2431b
Pool <35841895+Mirania@users.noreply.github.com>
2019-04-03 10:49:44 +0100

    Update Main.java

--- src/imagepreview/Main.java
+++ src/imagepreview/Main.java
@@ -11,7 +11,6 @@ public class Main {
 			System.out.println("- Full path to image file");
 		} else {
 			File file = new File(args[0]);
-			System.out.println(args[0]);
 			new Cropper().printImagePreview(file);
 		}
 	}
